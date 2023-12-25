7e3bc05d48d92ccc5d4936a334d1aecbc469d6ec
mikk369 <jogdial25@gmail.com>
2023-12-25 20:19:19 +0200

    added primeicons for mainNav

--- client/src/components/MainNav.vue
+++ client/src/components/MainNav.vue
@@ -2,43 +2,55 @@
   <nav>
     <ul class="nav-list">
       <li class="side-nav-links">
-        <router-link class="link-color-second" to="/admin"> Home</router-link>
+        <router-link class="link-color-second" to="/admin"
+        :class="{'active-link': $route.path === '/admin' }">
+          <i class="pi pi-home" style="font-size: 1.5rem">
+        </i> <span>Home</span></router-link>
       </li>
       <li class="side-nav-links">
         <router-link class="link-color-second" to="/lisapostitus"
-          >Postitused</router-link
+        :class="{'active-link': $route.path === '/lisapostitus' }">
+          <i class="pi pi-envelope" style="font-size: 1.5rem">
+        </i><span>Postitused</span></router-link
         >
       </li>
       <li class="side-nav-links">
         <router-link class="link-color-second" to="/kontaktideinfo"
-          >Kontaktid</router-link
-        >
+        :class="{'active-link': $route.path === '/kontaktideinfo' }">
+          <i class="pi pi-user-plus" style="font-size: 1.5rem">
+        </i><span>Kontaktid</span></router-link>
       </li>
       <li class="side-nav-links">
         <router-link class="link-color-second" to="/hoolekoguinfo"
-          >Hoolekogu</router-link
+        :class="{'active-link': $route.path === '/hoolekoguinfo' }">
+          <i class="pi pi-table" style="font-size: 1.5rem">
+        </i><span>Hoolekogu</span></router-link
         >
       </li>
       <li class="side-nav-links">
         <router-link class="link-color-second" to="/ruhmadeinfo"
-          >Rühmad</router-link
-        >
+        :class="{'active-link': $route.path === '/ruhmadeinfo' }">
+          <i class="pi pi-users" style="font-size: 1.5rem">
+        </i><span>Rühmad</span></router-link>
       </li>
       <li class="side-nav-links">
         <router-link class="link-color-second" to="/projektideinfo"
-          >Projektid</router-link
-        >
+        :class="{'active-link': $route.path === '/projektideinfo' }">
+          <i class="pi pi-book" style="font-size: 1.5rem">
+        </i><span>Projektid</span></router-link>
       </li>
       <li class="side-nav-links">
         <router-link class="link-color-second" to="/dokumentideinfo"
-          >Dokumendid</router-link
-        >
+        :class="{'active-link': $route.path === '/dokumentideinfo' }">
+          <i class="pi pi-file" style="font-size: 1.5rem">
+        </i><span>Dokumendid</span></router-link>
       </li>
     </ul>
   </nav>
 </template>
 
 <script>
+import 'primeicons/primeicons.css';
 export default {
   name: 'MainNav',
   data() {
@@ -69,10 +81,13 @@ export default {
   text-decoration: none;
 }
 
-.side-nav-links :hover,
-.active:link,
-.active:visited {
+
+.active-link {
   background-color: aliceblue;
   border-radius: 9px;
+}
+
+.active-link i {
+  color: #3498db;
 }
 </style>
