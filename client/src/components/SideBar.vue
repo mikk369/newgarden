2a3ea6b414a6aaf1c9514ade87dd8acc88b67bed
mikk369 <jogdial25@gmail.com>
2024-01-20 13:19:55 +0200

    removed rounded corners from sidebar

--- client/src/components/SideBar.vue
+++ client/src/components/SideBar.vue
@@ -23,10 +23,9 @@ aside {
   background-color: #fff;
   padding: 3.2rem 2.4rem;
   border: 1px solid aliceblue;
-  border-top-right-radius: 3rem; /* Adjust as needed */
-  border-bottom-right-radius: 3rem; /* Adjust as needed */
   overflow: hidden;
   grid-row: 1 / -1;
   background-color: aliceblue;
+  border-right: 1px solid var(--color-grey-100);
 }
 </style>
