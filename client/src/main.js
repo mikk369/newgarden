24b21ccbd2582f638333c3f3ca8946b179484cf7
mikk369 <jogdial25@gmail.com>
2024-01-23 17:04:24 +0200

    added primevue blue theme

--- client/src/main.js
+++ client/src/main.js
@@ -6,8 +6,8 @@ import HeaderView from './components/HeaderView.vue';
 import VueGtag from 'vue-gtag';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import 'primevue/resources/themes/saga-blue/theme.css';
 import 'primeicons/primeicons.css';
-import 'primevue/resources/themes/saga-blue/theme.css'
 import PrimeVue from 'primevue/config';
 
 const app = createApp(App);
