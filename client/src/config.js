b91d78fdfb11907d54425ebec0da3aa24bbce668
mikk369 <mikk.ereline@vikk.ee>
2024-02-07 20:10:18 +0200

    updated baseUrlApi

--- client/src/config.js
+++ client/src/config.js
@@ -2,7 +2,7 @@ const config = {
   baseUrl: 'http://localhost:8000/',
   baseUrlApi: 'http://localhost:8000/api',
   //  baseUrl = 'https://testbackend.pailasteaed.ee/'
-  //  baseUrl = 'https://testbackend.pailasteaed.ee/api'
+  //  baseUrlApi = 'https://testbackend.pailasteaed.ee/api'
 };
 
 export default config;
